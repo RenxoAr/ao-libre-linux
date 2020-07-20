fc6e9bc6bedc4be91ce206aa07814d2bbcad2d0e
EmilianoSalcedo <5517717+EmilianoSalcedo@users.noreply.github.com>
2020-07-20 18:32:33 -0300

    Agregado el output para unificacion y facilidad de modificaciones

--- install.sh
+++ install.sh
@@ -4,10 +4,21 @@ dirtemp="${dirtemp:-$HOME/.ao-libre-linux}"
 repo="${repo:-RenxoAr/ao-libre-linux}"
 remote="${remote:-https://github.com/${repo}.git}"
 branch="${branch:-master}"
+separador=$(printf "%*s\n" $(tput cols) " " | tr ' ' '=')
+texto_string(){ printf "%0*s\n" $(( $(tput cols) / 2 )) "${@}"; }
 
 git clone --branch "${branch}" "${remote}" "${dirtemp}"
 
-sh "${dirtemp}"/wine-installer.sh
+echo "${separador}"
+texto_string "INSTALANDO"
+echo "${separador}"
+
+sh "${dirtemp}"/dependencies.sh
 sh "${dirtemp}"/wine-config.sh
 sh "${dirtemp}"/cliente-installer.sh
+
+echo "${separador}"
+texto_string "FINALIZADO"
+echo "${separador}"
+
 sh "${dirtemp}"/run.sh
