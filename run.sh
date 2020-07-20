0a52441245fa68a9007159b6507d958e9a0cbc49
EmilianoSalcedo <5517717+EmilianoSalcedo@users.noreply.github.com>
2020-07-20 00:40:40 -0300

    Me olvide trasladar una variable (necesito cafe)

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 export prefix="${HOME}/.wine/wineprefix"
 prefixAO="${prefix}/Argentum/drive_c/Program Files/Argentum Online Libre/Launcher/Cliente"
 prefixVM="Argentum"
+vmcheck="$(cat /sys/class/dmi/id/product_name)"
 
 if [ "${vmcheck}" = "VirtualBox ]; then
     WINEPREFIX="${prefix}/${prefixVM}" winetricks videomemorysize=512
