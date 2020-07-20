bb010ba3951c9318849adcd5defdc173e43cf5cc
EmilianoSalcedo <5517717+EmilianoSalcedo@users.noreply.github.com>
2020-07-20 02:56:34 -0300

    Ya no es necesario el if

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ prefixAO="${prefix}/Argentum/drive_c/Program Files/Argentum Online Libre/Launche
 prefixVM="Argentum"
 vmcheck="$(cat /sys/class/dmi/id/product_name)"
 
-if [ "${vmcheck}" = "VirtualBox" ]; then WINEPREFIX="${prefix}/${prefixVM}" winetricks videomemorysize=512; fi
+[ "${vmcheck}" = "VirtualBox" ] && WINEPREFIX="${prefix}/${prefixVM}" winetricks videomemorysize=512
 
 WINEPREFIX="${prefix}/Argentum" WINEDEBUG=heap+all wine "${prefixAO}/Argentum.exe"
