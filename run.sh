fa08fdc1eaa5944bcefacf5931ea91280452515f
EmilianoSalcedo <5517717+EmilianoSalcedo@users.noreply.github.com>
2020-07-20 00:37:00 -0300

    Corregido y revertido para que chequee correctamenete si se esta en una VM luego de instalar el cliente

--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@
 
 export prefix="${HOME}/.wine/wineprefix"
 prefixAO="${prefix}/Argentum/drive_c/Program Files/Argentum Online Libre/Launcher/Cliente"
+prefixVM="Argentum"
 
-WINEPREFIX="${prefix}/Argentum" WINEDEBUG=heap+all wine "${prefixAO}/Argentum.exe"
+if [ "${vmcheck}" = "VirtualBox ]; then
+    WINEPREFIX="${prefix}/${prefixVM}" winetricks videomemorysize=512
+else
+    WINEPREFIX="${prefix}/Argentum" WINEDEBUG=heap+all wine "${prefixAO}/Argentum.exe"
+fi
