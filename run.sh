f1386201cb411a707e7da4852902e062eb54bf18
RenxoAr <54463914+RenxoAr@users.noreply.github.com>
2020-07-20 02:25:43 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ prefixAO="${prefix}/Argentum/drive_c/Program Files/Argentum Online Libre/Launche
 prefixVM="Argentum"
 vmcheck="$(cat /sys/class/dmi/id/product_name)"
 
-if [ "${vmcheck}" = "VirtualBox ]; then
+if [ "${vmcheck}" = "VirtualBox" ]; then
     WINEPREFIX="${prefix}/${prefixVM}" winetricks videomemorysize=512
 else
     WINEPREFIX="${prefix}/Argentum" WINEDEBUG=heap+all wine "${prefixAO}/Argentum.exe"
