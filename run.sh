1137d26e1d1f033058da29ac8997373634154fe6
RenxoAr <54463914+RenxoAr@users.noreply.github.com>
2020-07-20 19:18:53 -0300

    Update run.sh
    Muevo el check de vbox a la instalación del cliente, es innecesario correrlo cada vez que se ejecuta el cliente.

--- run.sh
+++ run.sh
@@ -3,8 +3,5 @@
 export prefix="${HOME}/.wine/wineprefix"
 prefixAO="${prefix}/Argentum/drive_c/Program Files/Argentum Online Libre/Launcher/Cliente"
 prefixDirAO="${prefix}/Argentum"
-vmcheck="$(cat /sys/class/dmi/id/product_name)"
-
-[ "${vmcheck}" = "VirtualBox" ] && WINEPREFIX="${prefixDirAO}" winetricks videomemorysize=512
 
 WINEPREFIX="${prefixDirAO}" WINEDEBUG=heap+all wine "${prefixAO}/Argentum.exe"
